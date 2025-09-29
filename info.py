d215197198cce9cc3681880a12f6b5a89f318929
skomar70 <mosharofroydev@gmail.com>
2025-09-29 03:15:40 +0000

    Update info.py
    Signed-off-by: skomar70 <mosharofroydev@gmail.com>

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '24776633'))
+API_HASH = environ.get('API_HASH', '57b1f632044b4e718f5dce004a988d69')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
@@ -167,4 +167,5 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
